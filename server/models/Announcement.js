8e168f378976e0177e2ec7dcbed662bafbea3f99
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-07-02 13:15:32 +0800

    chore: change location of team utils

--- server/models/Announcement.js
+++ server/models/Announcement.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const { generateShortId } = require("../middleware/team");
+const { generateShortId } = require("../utils/team");
 
 const announcementSchema = new Schema(
     {
